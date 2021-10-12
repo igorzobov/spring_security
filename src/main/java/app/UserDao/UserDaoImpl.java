31e22f0fb21a3d8d4f51e56de74951d1096e062e
igorzobov <igor_zobov@mail.ru>
2021-10-12 19:58:55 +0700

    added JDBCTemplate

--- src/main/java/app/UserDao/UserDaoImpl.java
+++ src/main/java/app/UserDao/UserDaoImpl.java
@@ -1,44 +1,46 @@
 package app.UserDao;
 
 import app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+//@PropertySource("classpath:db.properties")
 public class UserDaoImpl implements UserDao {
-    private static long ID;
-    List<User> userList;
-    {
-        userList = new ArrayList<>();
-        userList.add(new User(++ID, "Igor", 33, "igor_zobov@mail.ru"));
-        userList.add(new User(++ID, "Egor", 20, "email@egor.com"));
-        userList.add(new User(++ID, "Ivor", 45, "ivor@mail.ru"));
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<User> getUserList() {
-        return userList;
+        return jdbcTemplate.query("SELECT * FROM users", new BeanPropertyRowMapper<>(User.class));
     }
 
     public User show(long id) {
-        return userList.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM users WHERE id=?",
+                new Object[]{id}, new BeanPropertyRowMapper<>(User.class))
+                .stream().findFirst().orElse(null);
     }
 
     public void save(User user) {
-        user.setId(++ID);
-        userList.add(user);
+        jdbcTemplate.update("INSERT INTO users VALUES (1, ?, ?, ?)",
+                user.getName(), user.getAge(), user.getEmail());
     }
 
     public void update(long id, User updatedUser) {
-        User userToBeUpdated = show(id);
-
-        userToBeUpdated.setName(updatedUser.getName());
-        userToBeUpdated.setAge(updatedUser.getAge());
-        userToBeUpdated.setEmail(updatedUser.getEmail());
+        jdbcTemplate.update("UPDATE users SET name=?, age=?, email=? WHERE id=?",
+                updatedUser.getName(), updatedUser.getAge(), updatedUser.getEmail(), id);
     }
 
     public void delete(long id) {
-        userList.removeIf(u -> u.getId() == id);
+        jdbcTemplate.update("DELETE FROM users WHERE id=?", id);
     }
 }
